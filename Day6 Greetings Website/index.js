e285aab53920a0369c394891cc2a4cff83145230
Siddhi-Sovle <116745430+Siddhi-Sovle@users.noreply.github.com>
2023-01-18 22:45:28 +0530

    Add files via upload

---
+++ Day6 Greetings Website/index.js
@@ -0,0 +1,51 @@
+$(document).click(function(){
+
+  $(".gift").css("background-color","yellow");
+  $(".first").text("Happy Makar Sankranti to you!!🪁🪁🪁");
+
+  $(".gift").animate({
+    height:'400px',
+    width : '50%',
+    left : '300px'
+
+  });
+
+  $(".gift").animate({
+    height:'450px',
+    width : '50%',
+    left : '300px'
+
+  });
+
+
+  $(".first").animate({
+    fontSize : '55px'
+
+  });
+
+  $(".first").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+ $(".first").animate({
+   fontSize : '60px'
+ });
+
+ $(".first").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+ $(".first").animate({
+   fontSize : '65px'
+ });
+
+ $(".first").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+ $(".first").animate({
+   fontSize : '70px'
+ });
+
+ $(".first").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
+
+});
